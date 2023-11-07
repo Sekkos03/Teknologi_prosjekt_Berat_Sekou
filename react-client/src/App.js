5299919a994eb10a2e7cc577a7791693b4ab777f
borat <berat-murseli@hotmail.com>
2023-11-07 15:04:49 +0100

    fikset kode

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,10 +1,16 @@
-import "./styles.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,67 +1,12 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-  createRoutesFromElements
-} from "react-router-dom";
-import Support from "./routes/Support";
-import Home from "./routes/Home";
-import Navbar from "./components/Navbar";
+import ReactDOM from 'react-dom/client';
 import "./App.css";
 import "./index.css";
-import Class from "./routes/Class";
-import Submission from "./routes/Submission";
-import Chat from "./routes/Chat";
+import App from "./App";
 
-const AppLayout = () => (
-  <>
-    <Navbar />
-    <Outlet />
-  </>
-);
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route element={<AppLayout />}>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/products" element={<Products />} />
-//       <Route path="/reports" element={<Reports />} />
-//     </Route>
-//   )
-// );
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "support",
-        element: <Support />
-      },
-      {
-        path: "messages",
-        element: <Chat />
-      },
-      {
-        path: "submission",
-        element: <Submission />
-      },
-      {
-        path: "class",
-        element: <Class />
-      }
-    ]
-  }
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 );
